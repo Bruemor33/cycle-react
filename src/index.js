d5b88f405e8f5d3fbab2a23d568e25878928b3b1
Bruemor <arymer1@gmail.com>
2018-05-17 19:05:40 -0400

    Brought in react and it's working

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+    return <div>Hello react!</div>;
+}
+
+ReactDOM.render(<Index />, document.getElementById('index'));
+
+console.log("hello world");
+
+
